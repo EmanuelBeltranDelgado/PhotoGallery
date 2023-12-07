e1c7a8e85a7963f53bb0b12c5b8ae9de13998fd4
Emanuel <emanuel.beltran5853@alumnos.udg.mx>
2023-12-06 23:00:36 -0600

    Initial commit

---
+++ app/src/main/java/emanuel/beltran/android/photogallery/api/FlickrResponse.kt
@@ -0,0 +1,9 @@
+package emanuel.beltran.android.photogallery.api
+
+import com.squareup.moshi.JsonClass
+
+// FlickrResponse purpose: This class will map to the outermost object in the JSON data
+@JsonClass(generateAdapter = true)
+data class FlickrResponse(
+    val photos: PhotoResponse   // property to map to "photos" field, don't need @Json bc the names match
+)
